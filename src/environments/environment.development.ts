636377aaa5565f22f2b5a1d83e5d4bc4b3e43c8f
Eman-ibrahim-05 <emi3974133@gmail.com>
2025-09-19 20:43:50 +0300

    Final Update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     baseUrl: 'https://ecommerce.routemisr.com/api/v1',
-    frontend: 'http://localhost:4200',
+    frontend: 'https://vercel.com/eman-ibrahims-projects-1737707c/fresh-cart-qm98',
 };
