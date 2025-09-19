812476e9bc9e90e6cba5a63430fabcf0ad414ba0
Eman-ibrahim-05 <emi3974133@gmail.com>
2025-09-19 21:34:23 +0300

    Update git commit -m Update git commit -m Update

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,19 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'home', renderMode: RenderMode.Prerender },
+  { path: 'login', renderMode: RenderMode.Prerender },
+  { path: 'register', renderMode: RenderMode.Prerender },
+  { path: 'reset-password', renderMode: RenderMode.Prerender },
+
+  { path: 'products', renderMode: RenderMode.Server },
+  { path: 'categories', renderMode: RenderMode.Server },
+  { path: 'brands', renderMode: RenderMode.Server },
+  { path: 'cart', renderMode: RenderMode.Server },
+  { path: 'wish-list', renderMode: RenderMode.Server },
+  { path: 'allorders', renderMode: RenderMode.Server },
+  { path: 'product-details/:id', renderMode: RenderMode.Server },
+
+  { path: '**', renderMode: RenderMode.Server },
 ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   baseUrl: 'https://ecommerce.routemisr.com/api/v1',
-  frontend: 'https://vercel.com/eman-ibrahims-projects-1737707c/fresh-cart-qm98',
+  frontend: 'https://fresh-cart-fezu-git-master-eman-ibrahims-projects-1737707c.vercel.app/',
 };
